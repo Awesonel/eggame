b01b89697ebb705bb88426cf83acbb720fdaed19
isidorenko <isidorenko@solanteq.com>
2024-09-06 00:51:22 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/isidorenko/eggame/EggameApplication.kt
@@ -0,0 +1,11 @@
+package com.example.isidorenko.eggame
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EggameApplication
+
+fun main(args: Array<String>) {
+	runApplication<EggameApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/isidorenko/eggame/EggameApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.isidorenko.eggame
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EggameApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
